b0531e11a443ec1d59129af8cbc5526bcc5bac1a
stuart_zhang <stuartpage@qq.com>
2024-06-02 22:53:33 +0800

    完成

---
+++ README.md
@@ -0,0 +1,154 @@
+# grunt-connect-proxy-mock
+
+给`grunt`的`Dev Server`提供了
+
+1. `Mock`功能 — 既能`mock`普通的`Restful API`，也能伪装`WebSocket Server`推送`ws`假数据。
+2. `Proxy`功能 — 额外配备了对真实后端响应数据结果集的篡改能力。
+
+的中间件。
+
+## 用法概述
+
+在`grunt-contrib-connect`插件配置结点`connect`下的自定义指令配置子节点内（例如，指令为`livereload`的配置结点），添加三个新配置节点`webSocketMockRouteRules`，`webMockRouteRules`和`webProxyRouteRules`。
+
+```javascript
+{
+    connect: {
+        livereload: {
+            // WebSocket Mock Server 的路由清单
+            webSocketMockRouteRules: [..., {
+                //【必填】监听 WebSocket 请求的 url 路径
+                pathname: string,
+                //【可选】WebSocket 连接成功后的回调函数
+                onConnection: (ws, wss) => void,
+                //【必填】当收到来自网页端 WebSocket 推送消息时的回调函数
+                // - 在 onMessage 回调函数体内，以 ws.send(string) 成员方法回发消息给网页端。
+                onMessage: (message, ws, wss) => void,
+                //【可选】WebSocket 连接失败时的回调函数
+                onError: (err, ws, wss) => void
+            }, ...],
+            // Web Mock Server 的路由清单
+            webMockRouteRules: [..., [
+                //【必填】监听 HTTP 请求的全大写方法名
+                method, 
+                //【必填】监听 HTTP 请求的 url 路径。而且，支持
+                // - 路径参数       req.params 
+                // - 查询字符串参数 req.query
+                // - 表单参数       req.body
+                pathname, 
+                //【必填】Mock Server 中间件反复给网页端的假数据
+                // - 在回调函数内，不需要显式地 res.send() 回发数据集。将回发消息作为函数返回值返回即可
+                object | (req, res) => object
+            ], ...],
+            // Web Proxy Server 的路由清单
+            webProxyRouteRules: [..., [
+                //【必填】监听 HTTP 请求的全大写方法名
+                method, 
+                //【必填】监听 HTTP 请求的 url 路径。
+                pathname, 
+                //【可选】监听 HTTP 请求的 url 查询字符串参数
+                // - 若指令，必须全部出现在被拦截的请求体内
+                query, 
+                //【必填】真响应数据集的篡改函数
+                // - buffer 内容需手工转为字符串，以备篡改使用。
+                // - 在回调函数内，不需要显式地 res.send() 回发数据集。将回发消息作为函数返回值返回即可
+                (buffer: Buffer, req, res) => string | Buffer
+            ], ...]
+        }
+    }
+}
+```
+
+### 开启`Proxy Dev Server`中间件的先决条件
+
+第一，在宿主工程内，与`grunt-connect-proxy-mock`依赖项平级安装`peer dependency`依赖项`grunt-connect-proxy@^0.2.0`。
+
+第二，在`connect`中相同的指令配置结点下，给`grunt-connect-proxy`插件添加配置结点`proxies`
+
+```javascript
+{
+    connect: {
+        livereload: {
+            proxies: [..., {
+                //【必填】监听 HTTP 请求的 url 路径。
+                context: string,
+                //【必填】转发给后端真实服务的主机域名
+                host: string,
+                //【必填】转发给后端真实服务的端口号
+                port: number,
+                //【可选】转发后端真实服务是否是 ssl 的
+                https: boolean
+            }, ...]
+        }
+    }
+}
+```
+
+## 完整的`Gruntfile`配置例程
+
+```javascript
+{
+    // 
+    // 供 grunt-connect 插件的配置结点
+    //
+    connect: {
+        livereload: {
+            // 
+            // 供 grunt-connect-proxy 插件的配置结点
+            //
+            proxies: [..., {
+                //【必填】监听 HTTP 请求的 url 路径。
+                context: string,
+                //【必填】转发给后端真实服务的主机域名
+                host: string,
+                //【必填】转发给后端真实服务的端口号
+                port: number,
+                //【可选】转发后端真实服务是否是 ssl 的
+                https: boolean
+            }, ...],
+            // 
+            // 供 grunt-connect-proxy-mock 插件的（多个）配置结点
+            //
+            // WebSocket Mock Server 的路由清单
+            webSocketMockRouteRules: [..., {
+                //【必填】监听 WebSocket 请求的 url 路径
+                pathname: string,
+                //【可选】WebSocket 连接成功后的回调函数
+                onConnection: (ws, wss) => void,
+                //【必填】当收到来自网页端 WebSocket 推送消息时的回调函数
+                // - 在 onMessage 回调函数体内，以 ws.send(string) 成员方法回发消息给网页端。
+                onMessage: (message, ws, wss) => void,
+                //【可选】WebSocket 连接失败时的回调函数
+                onError: (err, ws, wss) => void
+            }, ...],
+            // Web Mock Server 的路由清单
+            webMockRouteRules: [..., [
+                //【必填】监听 HTTP 请求的全大写方法名
+                method, 
+                //【必填】监听 HTTP 请求的 url 路径。而且，支持
+                // - 路径参数       req.params 
+                // - 查询字符串参数 req.query
+                // - 表单参数       req.body
+                pathname, 
+                //【必填】Mock Server 中间件反复给网页端的假数据
+                // - 在回调函数内，不需要显式地 res.send() 回发数据集。将回发消息作为函数返回值返回即可
+                object | (req, res) => object
+            ], ...],
+            // Web Proxy Server 的路由清单
+            webProxyRouteRules: [..., [
+                //【必填】监听 HTTP 请求的全大写方法名
+                method, 
+                //【必填】监听 HTTP 请求的 url 路径。
+                pathname, 
+                //【可选】监听 HTTP 请求的 url 查询字符串参数
+                // - 若指令，必须全部出现在被拦截的请求体内
+                query, 
+                //【必填】真响应数据集的篡改函数
+                // - buffer 内容需手工转为字符串，以备篡改使用。
+                // - 在回调函数内，不需要显式地 res.send() 回发数据集。将回发消息作为函数返回值返回即可
+                (buffer: Buffer, req, res) => string | Buffer
+            ], ...]
+        }
+    }
+}
+```
---
+++ index.js
@@ -0,0 +1,213 @@
+module.exports = function(grunt) {
+	grunt.registerTask('configureProxyMock', '增强devServer实现Restful API的Mock功能', function(config) {
+		var configKey = 'connect.' + (config || 'livereload');
+		var connectOptions = grunt.config(configKey);
+		if (!connectOptions && config != 'livereload') {
+			connectOptions = grunt.config('connect.livereload');
+		}
+		if (!connectOptions) {
+			grunt.log.error('No connect configuration found.');
+			return;
+		}
+		var R = require('ramda');
+		/* {
+			webSocketMockRouteRules: [{
+				pathname: string,
+				onConnection: (ws, wss) => void,
+				onMessage: (message, ws, wss) => void,
+				onError: (err, ws, wss) => void
+			}, ...]
+		} */
+		R.is(Array)(connectOptions.webSocketMockRouteRules) && (function(){
+			var webSocketServer = R.partial(function onCreateServer(routeRules/* [{
+				pathname: string,
+				onConnection: (ws, wss) => void,
+				onMessage: (message, ws, wss) => void,
+				onError: (err, ws, wss) => void,
+				wss,
+			}, ...] */, server, connect, options){
+				var WebSocket = require('ws');
+				R.forEach(function(routeRule){
+					routeRule.wss = new WebSocket.Server({ noServer: true });
+					routeRule.wss.on('connection', function(ws){
+						ws.on('error', function(err){
+							grunt.log.warn('[configureProxyMock][webSocketServer][onError]', err);
+							typeof routeRule.onError == 'function' && routeRule.onError(err, ws, routeRule.wss);
+						}).on('message', function(message){
+							typeof routeRule.onMessage == 'function' && routeRule.onMessage(message, ws, routeRule.wss);
+						});
+						typeof routeRule.onConnection == 'function' && routeRule.onConnection(ws, routeRule.wss);
+					});
+				})(routeRules);
+				server.on('upgrade', function(request, socket, head){
+					var matchRouteRules = R.pipe(
+						R.filter(R.propSatisfies(R.equals(request.url), 'pathname')),
+						R.forEach(function(routeRule){
+							routeRule.wss.handleUpgrade(request, socket, head, function(ws){
+								routeRule.wss.emit('connection', ws, request);
+							});
+						})
+					)(routeRules);
+					R.isEmpty(matchRouteRules) && socket.destroy();
+				});
+			}, [connectOptions.webSocketMockRouteRules]);
+			if (typeof connectOptions.options.onCreateServer == 'function') {
+				var onCreateServer_ = connectOptions.options.onCreateServer.bind(connectOptions);
+			}
+			connectOptions.options.onCreateServer = function(server, connect, options){
+				onCreateServer_ && onCreateServer_(server, connect, options);
+				webSocketServer(server, connect, options);
+			};
+			grunt.config(configKey + '.options.onCreateServer', connectOptions.options.onCreateServer);
+		})();
+		/*
+			webMockRouteRules: [ [method, pathname, object | callback], ... ]
+		 */
+		R.is(Array)(connectOptions.webMockRouteRules) && (function(){
+			var webMockServer = R.partial(function middleware(routeRules/* [
+				[method, pathname, object | callback],
+			] */, connect, options, middleware){
+				var bodyParser = require('body-parser');
+				var pathParser = require('path-parser');
+				R.forEach(R.apply(function(method, pathName, response){
+					middleware.unshift(function(req, res, next){
+						if (req.method == method && testPathName()) {
+							buildQuery();
+							res.setHeader('Content-Type', 'application/json');
+							res.write(R.pipe(
+								R.when(
+									R.is(Function),
+									R.apply(R.__, [req, res])
+								),
+								R.unary(R.bind(JSON.stringify, JSON))
+							)(response));
+							return res.end();
+						}
+						return next();
+						function testPathName(){
+							if (req._parsedUrl.pathname == pathName) {
+								return true;
+							}
+							var params = new pathParser.Path(pathName).test(req._parsedUrl.pathname);
+							if (params) {
+								req.params = params;
+								return true;
+							}
+							return false;
+						}
+						function buildQuery(){
+							req.query = req.query || {};
+							var searchParams = new URL('http://127.0.0.1' + req.originalUrl).searchParams;
+							R.forEach(function(key){
+								var values = searchParams.getAll(key);
+								if (values.length == 1) {
+									req.query[key] = values[0];
+								} else if (values.length > 1) {
+									req.query[key] = values;
+								}
+							})(searchParams.keys());
+						}
+					});
+				}))(routeRules);
+				middleware.unshift(bodyParser.urlencoded({extended: false}));
+				middleware.unshift(bodyParser.json({limit: '150mb'}));
+				return middleware;
+			}, [connectOptions.webMockRouteRules]);
+			if (typeof connectOptions.options.middleware == 'function') {
+				var middleware_ = connectOptions.options.middleware.bind(connectOptions);
+			}
+			connectOptions.options.middleware = function(connect, options, middleware){
+				middleware = middleware_ ? middleware_(connect, options, middleware) : middleware;
+				return webMockServer(connect, options, middleware);
+			};
+			grunt.config(configKey + '.options.middleware', connectOptions.options.middleware);
+		})();
+		/*
+			webProxyRouteRules: [ [method, pathname, query, object | callback], ... ]
+		 */
+		R.is(Array)(connectOptions.webProxyRouteRules) && (function(){
+			var webProxyServer = R.partial(function middleware(routeRules/* [
+				[method, pathname, query, object | callback],
+			] */, connect, options, middleware){
+				var zlib = require('zlib');
+				var util = require('util');
+				var proxyUtils = require('grunt-connect-proxy/lib/utils');
+				options.selfHandleResponse = true;
+				middleware.push(proxyUtils.proxyRequest); // Setup the proxy
+				proxyUtils.proxies().forEach(function(proxy){
+					proxy.server.on('proxyReq', function(proxyReq, req, res, options){
+						proxyReq.removeAllListeners('response');
+						options.selfHandleResponse = true;
+						proxyReq.on('response', function(proxyRes){
+							R.pipe(
+								R.toPairs,
+								R.forEach(R.apply(function(key, value){
+									res.setHeader(key, value);
+								}))
+							)(proxyRes.headers);
+							proxy.server.emit('proxyRes', proxyRes, req, res);
+							if (!res.finished) {
+								proxyRes.on('end', function () {
+									proxy.server.emit('end', req, res, proxyRes);
+								});
+							} else {
+								proxy.server.emit('end', req, res, proxyRes);
+							}
+						});
+					});
+					proxy.server.on('proxyRes', function(proxyRes, req, res){
+						var searchParams = new URL('http://127.0.0.1' + req.originalUrl).searchParams;
+						var buffer;
+						proxyRes.on('data', function (chunk){
+							if (buffer) {
+								buffer = Buffer.concat([buffer, chunk]);
+							} else {
+								buffer = chunk;
+							}
+						});
+						proxyRes.on('end', function(){
+							var routeRule = R.find(R.apply(function(method, pathName, query, callback){
+								return req.method == method && req._parsedUrl.pathname == pathName && R.pipe(
+									R.toPairs,
+									R.ifElse(
+										R.isEmpty,
+										R.always(true),
+										R.all(R.apply(function(key, value){
+											return searchParams.get(key) == String(value);
+										}))
+									)
+								)(query);
+							}))(routeRules);
+							if (routeRule) {
+								if (proxyRes.headers['content-encoding'] == 'gzip') {
+									return util.promisify(zlib.gunzip)(buffer).then(function(buffer){
+										res.setHeader('content-encoding', 'identity');
+										return send(R.last(routeRule)(buffer, req, res));
+									});
+								}
+								return send(R.last(routeRule)(buffer, req, res));5
+							}
+							return send(buffer);
+							function send(result){
+								var resWrite = util.promisify(res.write.bind(res));
+								var resEnd = util.promisify(res.end.bind(res));
+								return (result ? resWrite(result) : Promise.resolve()).then(function(){
+									return resEnd();
+								});
+							}
+						});
+					});
+				});
+				return middleware;
+			}, [connectOptions.webProxyRouteRules]);
+			if (typeof connectOptions.options.middleware == 'function') {
+				var middleware_ = connectOptions.options.middleware.bind(connectOptions);
+			}
+			connectOptions.options.middleware = function(connect, options, middleware){
+				middleware = middleware_ ? middleware_(connect, options, middleware) : middleware;
+				return webProxyServer(connect, options, middleware);
+			};
+			grunt.config(configKey + '.options.middleware', connectOptions.options.middleware);
+		})();
+	});
+};
